3db5b818adcd71a5df382ec9e45bcd4a0a80154e
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-01 10:09:35 +0530

    manage course page desing

--- admin/includes/_manageCourse.php
+++ admin/includes/_manageCourse.php
@@ -1,1 +1,31 @@
 <h2>Manage course</h2>
+<hr>
+<div class="container-fluid">
+<div class="card mb-4">
+        <div class="card-body">
+            <table id="datatablesSimple">
+                <thead>
+                    <tr>
+                        <th>SNO</th>
+                        <th>Thumbnail</th>
+                        <th>Title & Category</th>
+                        <th>Lessons</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>SNO</th>
+                        <th>Thumbnail</th>
+                        <th>Title & Category</th>
+                        <th>Lessons</th>
+                        <th>Action</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
