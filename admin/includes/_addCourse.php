8c45b957892ef603cf8d62158e32a019071b140f
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-01 10:26:11 +0530

    course add page functionality -> completed

--- admin/includes/_addCourse.php
+++ admin/includes/_addCourse.php
@@ -1,4 +1,53 @@
 <?php
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        // grab the input field
+        $title = mysqli_real_escape_string($conn, $_POST['courseTitle']);
+        $courseCategory = mysqli_real_escape_string($conn, $_POST['courseCategory']);
+        $courseDesc = mysqli_real_escape_string($conn, $_POST['desc']);
+        $courseStatus = mysqli_real_escape_string($conn, $_POST['status']);
+
+        // image 
+        // support extension
+        $allowed_ext = array('png', 'jpg', 'jpeg');
+
+        $courseThumbnail = $_FILES['courseThumbnail']['name'];
+        $img_temp = $_FILES['courseThumbnail']['tmp_name'];
+        $image_size = $_FILES['courseThumbnail']['size'];
+        $target_dir = "../assets/thumbnail/$courseThumbnail";
+
+        $image_ext = explode('.', $courseThumbnail);
+        $image_ext = strtolower(end($image_ext));
+
+
+        // validate the form 
+        if(empty($title) || empty($courseCategory) || empty($courseDesc) || empty($courseStatus)) {
+            $formErr = "All fields are required!";
+        } else {
+            
+            if(in_array($image_ext, $allowed_ext)){
+                if($image_size <= 5000000){
+                    // image upload
+                    move_uploaded_file($img_temp, $target_dir);
+
+                    $addCourse = "INSERT INTO `courses` (`title`, `course_thumbnail`, `description`, `category_id`, `Status`, `created_at`) VALUES ('$title', '$courseThumbnail', '$courseDesc', '$courseCategory', '$courseStatus', current_timestamp());";
+                    $addResult = mysqli_query($conn, $addCourse);
+
+                    if(!$addResult) {
+                        die("Add Query failed: " . mysqli_error($conn));
+                    } else {
+                        header("Location: courses.php?added");
+                    }
+                    
+                }else {
+                    $imgErr = '<p class="text-danger">Image size is too large, image size should be less than 500KB.</p>';
+                }
+            }else{
+                $imgErr = '<p class="text-danger">Only .png, .jpg, .jpen and .gif allowed</p>';
+            } 
+
+        }
+    }
+
  // get categories
  $catQuery = "SELECT * FROM course_categories";
  $get_category = mysqli_query($conn, $catQuery);
@@ -11,6 +60,7 @@
 <hr>
 <div class="container">
     <div class="add-course w-50 m-auto border p-3">
+        <span class="text-danger mb-2 mt-2"><?= $formErr ?? null ?></span>
         <form action="" method="post" enctype="multipart/form-data">
             <div class="mb-2">
                 <label for="CourseTitle">Course Title</label>
@@ -41,6 +91,7 @@
                 <label for="courseThumbnail">Course Thumbnail</label>
                 <input type="file" name="courseThumbnail" class="form-control">
                 <span class="text-muted ms-2">Only .png, .jpg, .jpeg are allowed</span>
+                <span class="text-danger"><?= $imgErr ?? null ?></span>
             </div>
 
 
